c0ea07d87099881e907f97c8813e924ffd61f67d
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-01-28 11:49:55 +0100

    mysql delet 06

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM players WHERE name="David Silva"
 -- Opdracht 2 
-
--- Opdracht 3
-
+DELETE FROM players WHERE club="Willem II"
+-- Opdracht 
+DELETE FROM players WHERE club="FC Barcelona" AND nationality="Belgium"
 -- Opdracht 4
-
+DELETE FROM players WHERE age>="38"
 -- Opdracht 5
+DELETE FROM players WHERE id="167905" OR id="169595"
