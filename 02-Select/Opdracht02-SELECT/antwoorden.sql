07a5ef42527c041f72911d493e101eff5282178e
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-01-28 11:45:32 +0100

    mysql select 02

--- 02-Select/Opdracht02-SELECT/antwoorden.sql
+++ 02-Select/Opdracht02-SELECT/antwoorden.sql
@@ -1,12 +1,13 @@
 -- Opdracht 1 - voorbeeld
-SELECT * FROM jaar2016
+SELECT * FROM jaar2016;
 -- Opdracht 2 
-
+SELECT * FROM `jaar2015` WHERE rank = 1;
 -- Opdracht 3
-
+Select country, sore From jaar2016;
 -- Opdracht 4
-
+SELECT region From jaar2015;
 -- Opdracht 5
-
+Select region, socre from jaar2015;
 -- Opdracht 6
+Select score From jaar2016;
 
