3b636fadc55252466052eaf9f62585bac6d88581
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-01-28 11:46:42 +0100

    mysql select 03

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
-
--- Opdracht 2 
-
+SELECT * FROM jaar2015 WHERE rank="7"
+-- Opdracht 2
+SELECT country, rank FROM jaar2016 WHERE country="Netherlands"
 -- Opdracht 3
-
+SELECT country, score FROM jaar2015 WHERE country="Netherlands"
 -- Opdracht 4
-
+SELECT country, score FROM jaar2016 WHERE country="Netherlands"
 -- Opdracht 5
-
--- Opdracht 6 
-
--- Opdracht 7 
-
+SELECT country, region FROM jaar2015 WHERE region="North America"
+-- Opdracht 6
+SELECT country FROM jaar2016 WHERE rank="25"
+-- Opdracht 7
+SELECT country FROM jaar2016 WHERE score>"7087"
 -- Opdracht 8
-
+SELECT country, region FROM jaar2016 WHERE rank<="5"
 -- Opdracht 9
-
+SELECT score FROM jaar2015 WHERE country="Ireland"
 -- Opdracht 10
+SELECT country FROM jaar2016 where region="Latin America and Caribbean"
