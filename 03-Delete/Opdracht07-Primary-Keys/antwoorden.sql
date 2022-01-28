a930a0354641039cd177fa0aa37d4cb225488c65
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-01-28 11:50:31 +0100

    mysql delet 07

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,6 @@
 -- Opdracht 1
-
+SELECT * FROM players WHERE id="198329"
 -- Opdracht 2
-
+DELETE FROM players WHERE id = 198329
 -- Opdracht 3
+-- Het is gelukt om en primary key toe te kennen aan id
